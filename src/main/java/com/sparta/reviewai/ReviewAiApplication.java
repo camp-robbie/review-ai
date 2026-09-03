925bfc03bf129d9a8a9d13fa4b44683f2e051037
camp-robbie <camp.robbie.choi@gmail.com>
2026-09-03 11:38:48 +0900

    init commit

---
+++ src/main/java/com/sparta/reviewai/ReviewAiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.reviewai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewAiApplication.class, args);
+    }
+
+}
